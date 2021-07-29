5c680e73864d8064b9e12d4261ab8c6221cdc198
iwagner10 <32021070+iwagner10@users.noreply.github.com>
2021-07-29 14:17:41 -0400

    mini project quiz

---
+++ MiniQuizProject/MiniQuizProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniQuizProject
+//
+//  Created by Indi Wagner on 7/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
