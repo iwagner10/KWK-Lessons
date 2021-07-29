a272245be726180aa6c62fcdc95d0c9561cd8e2b
iwagner10 <32021070+iwagner10@users.noreply.github.com>
2021-07-29 10:35:03 -0400

    final mini project (with random song generator!)

--- MiniProject/MiniProject/ViewController.swift
+++ MiniProject/MiniProject/ViewController.swift
@@ -11,9 +11,13 @@ class ViewController: UIViewController {
     
 var facts = ["i go to phillips andover", "my favorite food is sushi", "i play ice hockey and lacrosse"]
     
+var songs = ["Brazil", "Briggs", "Alaska", "Good Nights", "Cigarette Daydreams", "Sedona", "Flashed Junk Mind", "Video Games", "Pierre", "Sensitivity", "Hallucinogenics", "Doses & Mimosas", "Paradise", "Trouble", "Buttercup", "hours", "Marlboro Nights", "Campus"]
+    
     @IBOutlet weak var fact1: UILabel!
     @IBOutlet weak var fact2: UILabel!
     @IBOutlet weak var fact3: UILabel!
+    
+    @IBOutlet weak var favSong: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,5 +30,11 @@ var facts = ["i go to phillips andover", "my favorite food is sushi", "i play ic
         fact3.text = facts[2]
     }
     
+    @IBAction func randomFavSong(_ sender: Any) {
+        let randomIndex = Int.random(in: 0..<songs.count)
+        let randomSong = songs[randomIndex]
+        
+        favSong.text = randomSong
+    }
 }
 
